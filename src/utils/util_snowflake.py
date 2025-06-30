bf3e84da8aaa1c71aa813d3b311cefce2da0f642
Jake Kealey <jake.kealey@nhs.net>
2025-06-30 15:28:58 +0100

    Add functionality to add query tag when creating session

--- src/utils/util_snowflake.py
+++ src/utils/util_snowflake.py
@@ -1,15 +1,17 @@
 from snowflake.snowpark.session import Session
+from snowflake.snowpark.context import get_active_session
 
-def snowpark_session_create(connection_params):
+def snowpark_session_create(connection_params, query_tag=False):
     """
     Create a Snowpark session using the provided configuration.
-    param config: Dictionary containing connection parameters:
+    connection_params: Dictionary containing connection parameters:
         - account: Snowflake account name
         - user: Snowflake username
         - authenticator: Snowflake authenticator (e.g., 'externalbrowser', 'snowflake')
         - warehouse: Snowflake warehouse
         - role: Snowflake role
         - database: Snowflake database
+    query_tag: Tag for logging and monitoring purposes
     Returns:
         - session: Snowpark session object
    """
