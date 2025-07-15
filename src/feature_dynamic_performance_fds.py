8980ec85a2228f09e17980ff86143e587954887f
Jake Kealey <jake.kealey@nhs.net>
2025-07-15 09:46:24 +0100

    Removed requirement for Date first seen as it threw the numbers off

--- src/feature_dynamic_performance_fds.py
+++ src/feature_dynamic_performance_fds.py
@@ -43,9 +43,7 @@ def performance_fds(df):
             )
         ) &
         not_(is_null(col("DATE_CANCERREFERRALTOTREATMENTPERIODSTARTDATE"))) &
-        not_(is_null(col("DATE_FDSPATHWAYENDDATE"))) &
-        #Additional requirement
-        not_(is_null(col("ORG_FIRSTSEEN_SITE")))   
+        not_(is_null(col("DATE_FDSPATHWAYENDDATE"))) 
     )
 
     #Set the Date fields
