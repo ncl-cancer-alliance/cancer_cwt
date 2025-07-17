d9f39e80ded217294bd74603bd27b0ec315b55e6
Jake Kealey <jake.kealey@nhs.net>
2025-07-17 08:54:32 +0100

    Updated base to use reference information for Primary Diagnosis, Cancer Referral Type, and Modality

--- docs/dynamic_cwt_base.sql
+++ docs/dynamic_cwt_base.sql
@@ -16,14 +16,19 @@ CREATE OR REPLACE DYNAMIC TABLE DATA_LAB_NCL_TRAINING_TEMP.CANCER_CWT.CWT_BASE (
     ORG_ACCOUNTABLETREATING_SITE VARCHAR, --Treating Organisation
 
     --Breakdowns Fields
+    ---Primary Diagnosis---
     CWT_PRIMARYDIAGNOSIS_CODE VARCHAR, --Diagnosis code
     CWT_PRIMARYDIAGNOSIS_DESC VARCHAR, --Diagnosis description
     CWT_PRIMARYDIAGNOSIS_GROUPING VARCHAR, --Grouping
     CWT_PRIMARYDIAGNOSIS_COSD_STAGEABLE BOOLEAN, --COSD
     CWT_PRIMARYDIAGNOSIS_RCRD_STAGEABLE BOOLEAN, --RCRD
 
+    ---Referral Type---
     CWT_CANCERREFERALTYPE_CODE CHAR(2), --Referral Type Code
     CWT_CANCERREFERALTYPE_DESC VARCHAR, --Referral Type Description
+
+    ---Modality---
+    CWT_MODALITY_DESC VARCHAR, --Modality Description
 
     --GP Fields
     PC_ICSREGISTRATION VARCHAR,
@@ -124,7 +129,7 @@ SELECT
     cwt.ORGTREATSTART AS ORG_ACCOUNTABLETREATING_SITE,
     
     --Breakdowns Fields
-    --Primary Diagnosis
+    ---Primary Diagnosis---
     cwt.PRIMARYDIAGNOSISICD AS CWT_PRIMARYDIAGNOSIS_CODE,
     ref_pd.FULL_DESCRIPTION AS CWT_PRIMARYDIAGNOSIS_DESC, 
     ref_pd.GROUPING AS CWT_PRIMARYDIAGNOSIS_GROUPING,
@@ -137,9 +142,12 @@ SELECT
         ELSE FALSE
     END AS CWT_PRIMARYDIAGNOSIS_RCRD_STAGEABLE,
     
-    --Cancer Referral Type
+    ---Cancer Referral Type---
     cwt.REFTYPE AS CWT_CANCERREFERALTYPE_CODE,
     ref_rt.FULL_DESCRIPTION AS CWT_CANCERREFERALTYPE_DESC,
+
+    ---Modality---
+    ref_mod.FULL_DESCRIPTION AS CWT_MODALITY_DESC,
 
     --GP Fields
     cwt.ICS_OF_REGISTRATION AS PC_ICSREGISTRATION,
@@ -198,32 +206,37 @@ SELECT
 FROM "Data_Store_Waiting".CWTDS."CWT001Data" cwt
 
 --Organisation Site and Trust Mapping
---Consultant Upgrades
+---Consultant Upgrades---
 LEFT JOIN org_par org_cu
 ON org_cu.ORG_SITE = cwt.ORGCONSUPGRADE
---First Seen
+---First Seen---
 LEFT JOIN org_par org_fs
 ON org_fs.ORG_SITE = cwt.ORGFIRSTSEEN
---Faster Diagnosis Standard
+---Faster Diagnosis Standard---
 LEFT JOIN org_par org_fdp
 ON org_fdp.ORG_SITE = cwt.ORGFDPEND
---Pathway Identifier
+---Pathway Identifier---
 LEFT JOIN org_par org_pi
 ON org_pi.ORG_SITE = cwt.ORGPPI
---Accountable Treating
+---Accountable Treating---
 LEFT JOIN org_par org_at
 ON org_at.ORG_SITE = cwt.ORGTREATSTART
 
 --Reference Information Joins
---Primary Diagnosis
+---Primary Diagnosis---
 LEFT JOIN DATA_LAB_NCL_TRAINING_TEMP.CANCER_CWT.CWT_REFERENCE_DATA ref_pd
 ON cwt.PRIMARYDIAGNOSISICD = ref_pd.CODE
 AND ref_pd.REFERENCE_CODE = 1
 
---Cancer Referral Type
+---Cancer Referral Type---
 LEFT JOIN DATA_LAB_NCL_TRAINING_TEMP.CANCER_CWT.CWT_REFERENCE_DATA ref_rt
 ON CAST(cwt.REFTYPE AS INT) = ref_rt.CODE
 AND ref_rt.REFERENCE_CODE = 4
+
+---Modality---
+LEFT JOIN DATA_LAB_NCL_TRAINING_TEMP.CANCER_CWT.CWT_REFERENCE_DATA ref_mod
+ON CAST(cwt.CANCERTREATMENTMODALITY AS INT) = ref_mod.CODE
+AND ref_mod.REFERENCE_CODE = 20
 
 --Qualify clause to filter out outdated records
 QUALIFY row_number() OVER (PARTITION BY cwt.RECORDID ORDER BY cwt."UniqSubmissionID" DESC) = 1
